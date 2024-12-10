ef4a42e4f48e25281c0fc7c4775a9838ce500de3
usounds <46260022+usounds@users.noreply.github.com>
2024-12-10 18:27:02 +0900

    HTMLタグの認識方法を変更

--- src/components/PostTextWithBold.tsx
+++ src/components/PostTextWithBold.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 
+// HTMLエスケープ用の関数
+const escapeHTML = (str: string): string => {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+};
+
 const PostTextWithBold = ({ postText }: { postText: string }) => {
-  // `[ ]` の間の部分を <strong> タグに置き換え
-  const processedText = postText
+  // HTMLエスケープ後にカスタム処理を適用
+  const processedText = escapeHTML(postText)
     .replace(/\[(.*?)\]/gs, (match, p1) => {
       return `<strong>${p1}</strong>`;
     })
-    // 指定されたURLの正規表現を使って URL を <a> タグに変換
     .replace(
       /https?:\/\/[-_.!~*'a-zA-Z0-9;\/?:\@&=+\$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g,
       (url) => `<a href="${url}" target="_blank" class="text-blue-700">${url}</a>`
     );
 
-  // dangerouslySetInnerHTMLを使ってHTMLをレンダリング
+  // dangerouslySetInnerHTMLを使ってレンダリング
   return (
     <div
       className="whitespace-pre-wrap break-words text-gray-800"
@@ -20,7 +29,5 @@ const PostTextWithBold = ({ postText }: { postText: string }) => {
     />
   );
 };
-
-
 
 export default PostTextWithBold;
