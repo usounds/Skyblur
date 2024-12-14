9668dceb4fd2410516a0a5baeb33952e59d4872a
usounds <46260022+usounds@users.noreply.github.com>
2024-12-14 22:38:09 +0900

    Revert "fix : デフォルトのフォントを削除"
    This reverts commit 1b2d55481812d833fd98b2dfdec850f463276c9f.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import Header from "@/components/Header";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Skyblur",
@@ -16,7 +28,7 @@ export default function RootLayout({
     <html lang="ja">
 
       <body
-        className={` antialiased bg-white `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white `}
       >
 
       <Header />
