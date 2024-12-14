1b2d55481812d833fd98b2dfdec850f463276c9f
usounds <grave.evarg@gmail.com>
2024-12-14 21:11:20 +0900

    fix : デフォルトのフォントを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Skyblur",
@@ -27,7 +15,7 @@ export default function RootLayout({
     <html lang="ja">
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white `}
+        className={` antialiased bg-white `}
       >
 
         {children}
