816c4210d8cc06f31ba97edd629d6ce6f687ed8d
usounds <grave.evarg@gmail.com>
2024-12-15 02:22:25 +0900

    fix : リファクタ

--- src/logic/LocaledDatetime.ts
+++ src/logic/LocaledDatetime.ts
@@ -1,18 +1,18 @@
 export const formatDateToLocale = (dateString: string) => {
-    const date = new Date(dateString);
-    let userLocale = 'en'; // default locale
-  
-    if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
-      userLocale = navigator.language; // ブラウザのロケールを取得
-    }
-    
-    return new Intl.DateTimeFormat(userLocale, {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      hour12: false, // 24時間表示
-    }).format(date);
-  };
+  const date = new Date(dateString);
+  let userLocale = 'en'; // default locale
+
+  if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
+    userLocale = navigator.language; // ブラウザのロケールを取得
+  }
+
+  return new Intl.DateTimeFormat(userLocale, {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false, // 24時間表示
+  }).format(date);
+};
