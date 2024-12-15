d5576076e5d4b594f3f29b7c487ad10485255f5b
usounds <grave.evarg@gmail.com>
2024-12-15 21:51:27 +0900

    feat : 404にヘッダを追加

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/Header";
+
 export default function NotFound() {
     return (
+    <>
+      <Header />
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1 className="text-black">404 - Page Not Found</h1>
         <p className="text-black">The page you are looking for doesn&apos;t exist.</p>
       </div>
+      </>
     );
   }
