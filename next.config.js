2bd0019cecd54c2745e8a8f50ec38cc515add497
usounds <46260022+usounds@users.noreply.github.com>
2024-12-15 13:25:17 +0900

    fix : images.domainsをremotePatternsに

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  // 必要な設定をここに追加します
-  reactStrictMode: true,  // 例: React Strict Mode を有効化
+  reactStrictMode: true,  
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'backet.skyblur.uk',
-        pathname: '/**', // パスも指定可能
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.bsky.app',
+        pathname: '/**', 
       },
     ],
-    domains: ['cdn.bsky.app'],
   },
 };
