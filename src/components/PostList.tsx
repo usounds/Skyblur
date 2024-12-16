46ad93d2b50c1c2121fde6b8daebd41ece56bbc9
usounds <46260022+usounds@users.noreply.github.com>
2024-12-17 01:50:25 +0900

    feat : アイコンもボタン化

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from "react";
 
-import { Button } from 'reablocks';
+import { Button, IconButton } from 'reablocks';
 
 type PostListProps = {
     handleEdit: (input: PostListItem) => void;
@@ -89,12 +89,12 @@ export const PostList: React.FC<PostListProps> = ({
     };
 
     // 削除確認ダイアログを閉じる関数
-    const handleCloseOverlay = (isDeleted : boolean) => {
+    const handleCloseOverlay = (isDeleted: boolean) => {
         if (isDeleted && selectedItem) {
             setIsModalOpen(false)
             setDeleteList(prevList => prevList.filter(item => item.blurATUri !== selectedItem.blurATUri));
         }
-        
+
         setSelectedItem(null);
     };
 
@@ -186,10 +186,10 @@ export const PostList: React.FC<PostListProps> = ({
                                 <PostTextWithBold postText={item.blur.text} isValidateBrackets={true} />
                             </div>
 
-                            <div className="flex justify-between gap-2 mt-2">
+                            <div className="flex justify-between items-center gap-2 mt-2">
                                 <div className="text-sm text-gray-400">{formatDateToLocale(item.blur.createdAt)}</div>
-                                <div className="flex gap-2">
-                                    <div className="text-sm text-red-500 mx-3" onClick={() => handleSelectItem(item)}>
+                                <div className="flex sm:gap-6 gap-4">
+                                    <IconButton size="small" variant="text" onClick={() => handleSelectItem(item)}>
                                         <Image
                                             src="https://backet.skyblur.uk/trash2.svg" // public フォルダ内のファイルは / からの相対パスで指定
                                             alt="Trash Icon"
@@ -197,8 +197,8 @@ export const PostList: React.FC<PostListProps> = ({
                                             height={20} // 必要に応じて高さを指定
                                             unoptimized
                                         />
-                                    </div>
-                                    <div onClick={() => handleEdit(item)} className="text-sm text-gray-500 mx-3">
+                                    </IconButton>
+                                    <IconButton size="small" variant="text" onClick={() => handleEdit(item)} >
                                         <Image
                                             src="https://backet.skyblur.uk/edit.svg" // public フォルダ内のファイルは / からの相対パスで指定
                                             alt="Export Icon"
@@ -206,19 +206,23 @@ export const PostList: React.FC<PostListProps> = ({
                                             height={20} // 必要に応じて高さを指定
                                             unoptimized
                                         />
-                                    </div>
-                                    <Link className="text-sm text-gray-500 mx-3" href={`${item.blurURL || ''}?q=preview`}>
-                                        <Image
-                                            src="https://backet.skyblur.uk/right-arrow.svg" // public フォルダ内のファイルは / からの相対パスで指定
-                                            alt="Export Icon"
-                                            width={20} // 必要に応じて幅を指定
-                                            height={20} // 必要に応じて高さを指定
-                                            unoptimized
-                                        />
-                                    </Link>
-                                    <a className="text-sm text-gray-600 ml-2 mr-2" href={item.postURL} target="_blank">
-                                        <svg className="h-5 w-5" width="20" height="20" viewBox="0 0 1452 1452" xmlns="http://www.w3.org/2000/svg"><path d="M725.669,684.169c85.954,-174.908 196.522,-329.297 331.704,-463.171c45.917,-43.253 98.131,-74.732 156.638,-94.443c80.779,-23.002 127.157,10.154 139.131,99.467c-2.122,144.025 -12.566,287.365 -31.327,430.015c-29.111,113.446 -96.987,180.762 -203.629,201.947c-36.024,5.837 -72.266,8.516 -108.726,8.038c49.745,11.389 95.815,32.154 138.21,62.292c77.217,64.765 90.425,142.799 39.62,234.097c-37.567,57.717 -83.945,104.938 -139.131,141.664c-82.806,48.116 -154.983,33.716 -216.529,-43.202c-28.935,-38.951 -52.278,-81.818 -70.026,-128.603c-12.177,-34.148 -24.156,-68.309 -35.935,-102.481c-11.779,34.172 -23.757,68.333 -35.934,102.481c-17.748,46.785 -41.091,89.652 -70.027,128.603c-61.545,76.918 -133.722,91.318 -216.529,43.202c-55.186,-36.726 -101.564,-83.947 -139.131,-141.664c-50.804,-91.298 -37.597,-169.332 39.62,-234.097c42.396,-30.138 88.466,-50.903 138.21,-62.292c-36.46,0.478 -72.702,-2.201 -108.725,-8.038c-106.643,-21.185 -174.519,-88.501 -203.629,-201.947c-18.762,-142.65 -29.205,-285.99 -31.328,-430.015c11.975,-89.313 58.352,-122.469 139.132,-99.467c58.507,19.711 110.72,51.19 156.637,94.443c135.183,133.874 245.751,288.263 331.704,463.171Z" fill="currentColor" /></svg>
-                                    </a>
+                                    </IconButton>
+                                    <IconButton size="small" variant="text"  >
+                                        <Link href={`${item.blurURL || ''}?q=preview`}>
+                                            <Image
+                                                src="https://backet.skyblur.uk/right-arrow.svg" // public フォルダ内のファイルは / からの相対パスで指定
+                                                alt="Export Icon"
+                                                width={20} // 必要に応じて幅を指定
+                                                height={20} // 必要に応じて高さを指定
+                                                unoptimized
+                                            />
+                                        </Link>
+                                    </IconButton>
+                                    <IconButton size="small" variant="text"  >
+                                        <a href={item.postURL} target="_blank">
+                                            <svg width="20" height="20" viewBox="0 0 1452 1452" xmlns="http://www.w3.org/2000/svg"><path d="M725.669,684.169c85.954,-174.908 196.522,-329.297 331.704,-463.171c45.917,-43.253 98.131,-74.732 156.638,-94.443c80.779,-23.002 127.157,10.154 139.131,99.467c-2.122,144.025 -12.566,287.365 -31.327,430.015c-29.111,113.446 -96.987,180.762 -203.629,201.947c-36.024,5.837 -72.266,8.516 -108.726,8.038c49.745,11.389 95.815,32.154 138.21,62.292c77.217,64.765 90.425,142.799 39.62,234.097c-37.567,57.717 -83.945,104.938 -139.131,141.664c-82.806,48.116 -154.983,33.716 -216.529,-43.202c-28.935,-38.951 -52.278,-81.818 -70.026,-128.603c-12.177,-34.148 -24.156,-68.309 -35.935,-102.481c-11.779,34.172 -23.757,68.333 -35.934,102.481c-17.748,46.785 -41.091,89.652 -70.027,128.603c-61.545,76.918 -133.722,91.318 -216.529,43.202c-55.186,-36.726 -101.564,-83.947 -139.131,-141.664c-50.804,-91.298 -37.597,-169.332 39.62,-234.097c42.396,-30.138 88.466,-50.903 138.21,-62.292c-36.46,0.478 -72.702,-2.201 -108.725,-8.038c-106.643,-21.185 -174.519,-88.501 -203.629,-201.947c-18.762,-142.65 -29.205,-285.99 -31.328,-430.015c11.975,-89.313 58.352,-122.469 139.132,-99.467c58.507,19.711 110.72,51.19 156.637,94.443c135.183,133.874 245.751,288.263 331.704,463.171Z" fill="currentColor" /></svg>
+                                        </a>
+                                    </IconButton>
                                 </div>
                             </div>
                         </div>
@@ -228,10 +232,10 @@ export const PostList: React.FC<PostListProps> = ({
                 </div>
 
 
-                {isModalOpen && selectedItem && 
-                                <DeleteModal content={selectedItem.blur.text} onConfirm={handleDeleteItem} onClose={handleCloseOverlay} />
-                                
-                            }
+                {isModalOpen && selectedItem &&
+                    <DeleteModal content={selectedItem.blur.text} onConfirm={handleDeleteItem} onClose={handleCloseOverlay} />
+
+                }
 
                 {!isLoading &&
                     <div className="flex justify-center gap-4 mt-6">
@@ -241,6 +245,6 @@ export const PostList: React.FC<PostListProps> = ({
                     </div>
                 }
 
-        </div >
+            </div >
         </>)
 }
