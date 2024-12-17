9dc020841b5e4077b8eaa5b03600e6234a9feb29
usounds <46260022+usounds@users.noreply.github.com>
2024-12-17 13:08:37 +0900

    Revert "feat : 投稿画面のテキストエリアの改善"
    This reverts commit bd1b8349715cbff2a2e32700283136b1a6310f76.

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -449,6 +449,9 @@ export const CreatePostForm: React.FC<CreatePostProps> = ({
                             }
                         </div>
 
+                        <div className="block text-sm text-gray-600 mt-1">{postText.length}/300</div>
+
+
                         <div className='mt-2'>{locale.CreatePost_Preview}</div>
                         <div className="block text-sm text-gray-400 mt-1">{locale.CreatePost_PreviewDescription}</div>
 
@@ -473,6 +476,8 @@ export const CreatePostForm: React.FC<CreatePostProps> = ({
                             max={10000}
                             isEnableBrackets={false}
                         />
+
+                        <div className="block text-sm text-gray-600 mt-1">{addText.length}/10000</div>
 
                         <div className="flex justify-center gap-4 mb-8">
                             {!warning &&
