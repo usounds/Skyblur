9110027b2f4d28aa5f135a081b967845ce6b590c
usounds <grave.evarg@gmail.com>
2024-12-30 21:44:39 +0900

    fix : Denyした場合の挙動の改善

--- src/pages/oauth/OauthCallback.tsx
+++ src/pages/oauth/OauthCallback.tsx
@@ -16,16 +16,18 @@ const OauthCallBack = () => {
     const setLoginXrpc = useXrpcStore((state) => state.setLoginXrpc);
     const setDid = useXrpcStore((state) => state.setDid);
     const setIsLoginProcess = useXrpcStore((state) => state.setIsLoginProcess);
+    const isLoginProcess = useXrpcStore((state) => state.isLoginProcess);
     const navigate = useNavigate();
     const locale = useLocaleStore((state) => state.localeData);
 
     useEffect(() => {
+        if(isLoginProcess) return
         setBlueskyLoginMessage('')
         setIsLoginProcess(true)
         const params = new URLSearchParams(location.hash.slice(1));
         console.log(params.size)
         if (params.size === 4) {
-            setBlueskyLoginMessage('認証に失敗しました')
+            setBlueskyLoginMessage('Negative Navigation')
             console.log(params)
             navigate('/login');
             return
@@ -43,28 +45,34 @@ const OauthCallBack = () => {
                 },
             });
 
-            // this is optional, but after retrieving the parameters, we should ideally
-            // scrub it from history to prevent this authorization state to be replayed,
-            // just for good measure.
-            history.replaceState(null, '', location.pathname + location.search);
+            try {
+                // this is optional, but after retrieving the parameters, we should ideally
+                // scrub it from history to prevent this authorization state to be replayed,
+                // just for good measure.
+                history.replaceState(null, '', location.pathname + location.search);
 
-            // you'd be given a session object that you can then pass to OAuthUserAgent!
-            const session = await finalizeAuthorization(params);
+                // you'd be given a session object that you can then pass to OAuthUserAgent!
+                const session = await finalizeAuthorization(params);
 
-            // now you can start making requests!
-            const agent = new OAuthUserAgent(session);
+                // now you can start making requests!
+                const agent = new OAuthUserAgent(session);
 
-            if (agent) {
-                const xrpc = new XRPC({ handler: agent });
+                if (agent) {
+                    const xrpc = new XRPC({ handler: agent });
 
-                setLoginXrpc(xrpc)
+                    setLoginXrpc(xrpc)
 
-                const ret = await xrpc.get("app.bsky.actor.getProfile", { params: { actor: agent.sub } })
-                setUserProf(ret.data)
-                setDid(agent.sub)
+                    const ret = await xrpc.get("app.bsky.actor.getProfile", { params: { actor: agent.sub } })
+                    setUserProf(ret.data)
+                    setDid(agent.sub)
+                    setIsLoginProcess(false)
+
+                    navigate('/');
+
+                }
+            } catch (e) {
                 setIsLoginProcess(false)
-
-                navigate('/');
+                setBlueskyLoginMessage(''+e)
 
             }
         };
