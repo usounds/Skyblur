36ed45521024df63e2c4c6c4ce2ee9c8ab549f7a
usounds <grave.evarg@gmail.com>
2025-01-16 13:00:04 +0900

    feat : マイページとカスタムフィードにページ遷移機能を追加

--- src/components/URLCopyButton.tsx
+++ src/components/URLCopyButton.tsx
@@ -2,25 +2,53 @@ import React from 'react';
 import { LuClipboardCheck } from "react-icons/lu";
 import { Button } from 'reablocks';
 import { useLocaleStore } from "@/state/Locale";
+import { FaRegWindowRestore } from "react-icons/fa6";
+import Link from 'next/link';
 
 interface URLCopyButtonProps {
     url: string;
 }
 
-const URLCopyButton: React.FC<URLCopyButtonProps> = ({url}) => {
-  const locale = useLocaleStore((state) => state.localeData);
+const URLCopyButton: React.FC<URLCopyButtonProps> = ({ url }) => {
+    const locale = useLocaleStore((state) => state.localeData);
     const handleCopy = () => {
         navigator.clipboard
             .writeText(url)
     };
 
     return (
-        <Button size="small" variant="outline" startAdornment={
-            <LuClipboardCheck
-                size={26} color="gray"
-            />} onClick={handleCopy}>
-            {locale.Menu_URLCopy}
-        </Button>
+        <>
+            <div className="flex gap-3">
+                <Link href={url} target="_blank">
+                <Button
+                    size="small"
+                    variant="outline"
+                    startAdornment={
+                        <FaRegWindowRestore
+                            size={26}
+                            color="gray"
+                        />
+                    }
+                >
+                    {locale.Menu_GoSite}
+                </Button>
+                </Link>
+                <Button
+                    size="small"
+                    variant="outline"
+                    startAdornment={
+                        <LuClipboardCheck
+                            size={26}
+                            color="gray"
+                        />
+                    }
+                    onClick={handleCopy}
+                >
+                    {locale.Menu_URLCopy}
+                </Button>
+            </div>
+        </>
+
     );
 };
 
