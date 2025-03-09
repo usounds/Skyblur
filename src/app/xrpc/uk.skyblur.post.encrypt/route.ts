efe18fc566a7388cf0828f388903dc9b56fb5cc6
usounds <46260022+usounds@users.noreply.github.com>
2025-03-10 00:55:36 +0900

    feat : 投稿別パスワード認証のセキュリティ向上

--- src/app/xrpc/uk.skyblur.post.encrypt/route.ts
+++ src/app/xrpc/uk.skyblur.post.encrypt/route.ts
@@ -1,8 +1,24 @@
 export const runtime = 'edge';
 import { NextRequest, NextResponse } from "next/server";
 import { deriveKey } from "@/logic/HandleEncrypt";
+import { verifyJWT } from "@/logic/HandleBluesky"
 
 export async function POST(req: NextRequest) {
+  const authorization = req.headers.get('Authorization') || ''
+  const decodeJWT = authorization.replace('Bearer ', '').trim()
+  const origin = req.headers.get('host') || ''
+  
+  try {
+    const result = await verifyJWT(decodeJWT, `did:web:${origin}`)
+
+    if (!result || !result.verified) {
+      return NextResponse.json({ error: "Invalid Token" }, { status: 400 });
+    }
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 400 });
+
+  }
+
   try {
     const { body, password } = await req.json();
     if (!body || !password) {
@@ -22,6 +38,6 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ encryptedText });
   } catch (error) {
-    return NextResponse.json({ error: "Encryption failed. "+error }, { status: 500 });
+    return NextResponse.json({ error: "Encryption failed. " + error }, { status: 500 });
   }
 }
