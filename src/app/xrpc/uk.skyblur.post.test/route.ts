0e76ae56625430986a3e5a016b809e612374b9bd
usounds <46260022+usounds@users.noreply.github.com>
2025-03-09 17:42:08 +0900

    feat : SALTの指定ミス

--- src/app/xrpc/uk.skyblur.post.test/route.ts
+++ src/app/xrpc/uk.skyblur.post.test/route.ts
@@ -2,6 +2,6 @@ export const runtime = 'edge';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const SALT = process.env.SALT || "salt";
+  const SALT = process.env.SECRET_KEY || "salt";
   return NextResponse.json({ salt: SALT });
 }
