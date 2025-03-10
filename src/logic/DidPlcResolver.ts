853649f1eefbad6331307f09ac018c99f67cee50
usounds <grave.evarg@gmail.com>
2025-03-10 09:14:33 +0900

    デバッグ

--- src/logic/DidPlcResolver.ts
+++ src/logic/DidPlcResolver.ts
@@ -7,12 +7,14 @@ export function getResolver() {
         didResolver: Resolver,
         options: DIDResolutionOptions
     ): Promise<DIDDocument> {
-
         const encodedDid = encodeURIComponent(did);
         const didUrl = `https://plc.directory/${encodedDid}`;
 
+        console.log('getResolver - fetch')
         const response = await fetch(didUrl);
         const didDoc = await response.json() as DIDDocument
+
+        console.log(didDoc)
 
         return didDoc
     }
