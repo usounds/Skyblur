37a767d66e1eef77d05e48f3a94120265ed5db84
usounds <grave.evarg@gmail.com>
2025-03-15 15:06:40 +0900

    frontendとbackendのリポジトリを統合

---
+++ backend/src/logic/JWTTokenHandler.ts
@@ -0,0 +1,62 @@
+
+import { Resolver, ResolverRegistry, DIDResolver, DIDDocument } from 'did-resolver'
+import { getResolver } from '../logic/DidPlcResolver'
+import { getResolver as getWebResolver } from 'web-did-resolver'
+import * as didJWT from 'did-jwt';
+const myResolver = getResolver()
+const web = getWebResolver()
+const resolver: ResolverRegistry = {
+    'plc': myResolver.DidPlcResolver as unknown as DIDResolver,
+    'web': web as unknown as DIDResolver,
+}
+export const resolverInstance = new Resolver(resolver)
+export type Service = {
+    id: string;
+    type: string;
+    serviceEndpoint: string | Record<string, any> | Array<Record<string, any>>;
+}
+
+export const verifyJWT = async (auth: string, audience:string) => {
+    const authorization = auth.replace('Bearer ', '').trim()
+    const decodedJWT = authorization.replace('Bearer ', '').trim()
+
+    const result = await didJWT.verifyJWT(decodedJWT, {
+        resolver: resolverInstance,
+        audience: audience
+    })
+
+    return result
+
+}
+export const fetchDiDDocument = async (did: string) => {
+    try {
+        const didDocument = await resolverInstance.resolve(did)
+        return didDocument
+    } catch (error) {
+        console.error('Error fetching service endpoint:', error);
+    }
+
+    
+};
+
+export const fetchServiceEndpoint = async (did: string) => {
+    try {
+        const response = await fetchDiDDocument(did);
+        if (!response) {
+            throw new Error('Invalid DID document response');
+        }
+
+        const didDocument = response as unknown as DIDDocument;
+
+        // didDocument.serviceが存在するかチェック
+        const service = didDocument.service?.find((s: Service) => s.id === '#atproto_pds');
+
+        if (service && service.serviceEndpoint) {
+            return service.serviceEndpoint;
+        } else {
+            throw new Error('Service with id #atproto_pds not found or no service endpoint available');
+        }
+    } catch (error) {
+        console.error('Error fetching service endpoint:', error);
+    }
+};
---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+## Getting Started
+
+First, run the development server:
+```
+npm install
+npm run dev
+```
+
+For Production build:
+```
+npm run build
+```
