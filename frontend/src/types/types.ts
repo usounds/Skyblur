53b968c331a48b5348effb29a7359e2e6d981dd5
usounds <46260022+usounds@users.noreply.github.com>
2025-08-10 01:18:43 +0900

    feat : メンション機能復活

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -152,7 +152,7 @@ export const customTheme: PartialReablocksTheme = {
         }
     },
 };
-
+export const MENTION_REGEX = /(^|\s|\()(@)([a-zA-Z0-9.-]+)(\b)/g
 export const TAG_REGEX =
   // eslint-disable-next-line no-misleading-character-class
   /(^|\s)[#＃]((?!\ufe0f)[^\s\u00AD\u2060\u200A\u200B\u200C\u200D\u20e2]*[^\d\s\p{P}\u00AD\u2060\u200A\u200B\u200C\u200D\u20e2]+[^\s\u00AD\u2060\u200A\u200B\u200C\u200D\u20e2]*)?/gu
