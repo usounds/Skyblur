c035c3e41818d85a9595b47e5c639369ad52e6c3
usounds <46260022+usounds@users.noreply.github.com>
2025-08-10 16:16:51 +0900

    feat : Profile取得をpublic.api.bsky.app経由に

--- frontend/src/logic/HandleOAuth.ts
+++ frontend/src/logic/HandleOAuth.ts
@@ -1,9 +1,8 @@
 "use client"
 import { fetchServiceEndpointWithCache } from "@/logic/HandleBluesky";
-import { Client } from '@atcute/client';
-import { ClientMetadata } from '@atcute/oauth-browser-client';
-import { OAuthUserAgent, configureOAuth, finalizeAuthorization, getSession } from '@atcute/oauth-browser-client';
 import { AppBskyActorDefs } from '@atcute/bluesky';
+import { Client, simpleFetchHandler } from '@atcute/client';
+import { ClientMetadata, OAuthUserAgent, configureOAuth, finalizeAuthorization, getSession } from '@atcute/oauth-browser-client';
 
 export async function handleOAuth(
   getClientMetadata: () => ClientMetadata | null | undefined,
@@ -51,7 +50,12 @@ export async function handleOAuth(
       const endPoint = await fetchServiceEndpointWithCache(agent.sub, true)
       setServiceUrl(endPoint || '')
       console.log(`${agent.sub} was successfully authenticated from ${endPoint}.`)
-      const userProfile = await rpc.get(`app.bsky.actor.getProfile`, {
+      const publicAgent = new Client({
+        handler: simpleFetchHandler({
+          service: 'https://public.api.bsky.app',
+        }),
+      })
+      const userProfile = await publicAgent.get(`app.bsky.actor.getProfile`, {
         params: {
           actor: agent.sub,
         },
@@ -91,7 +95,12 @@ export async function handleOAuth(
       const endPoint = await fetchServiceEndpointWithCache(agent.sub, false)
       setServiceUrl(endPoint || '')
       console.log(`${agent.sub} was restored (last active session) from ${endPoint}.`)
-      const userProfile = await rpc.get(`app.bsky.actor.getProfile`, {
+      const publicAgent = new Client({
+        handler: simpleFetchHandler({
+          service: 'https://public.api.bsky.app',
+        }),
+      })
+      const userProfile = await publicAgent.get(`app.bsky.actor.getProfile`, {
         params: {
           actor: agent.sub,
         },
