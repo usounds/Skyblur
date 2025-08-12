b74ccd7ec564c57ef55501f0c6596072f15c9840
usounds <46260022+usounds@users.noreply.github.com>
2025-08-12 18:47:44 +0900

    fix : 設定とログアウトにセパレータ配置

--- frontend/src/components/AvatorDropdownMenu.tsx
+++ frontend/src/components/AvatorDropdownMenu.tsx
@@ -80,6 +80,7 @@ export function AvatorDropdownMenu() {
                     <Menu.Item leftSection={<IconSettings size={18} />} onClick={handleSettings}>
                         {locale.Menu_Settings}
                     </Menu.Item>
+                    <Menu.Divider />
                     <Menu.Item leftSection={<CiLogout size={18} />} onClick={logout}>
                         {locale.Menu_Logout}
                     </Menu.Item>
