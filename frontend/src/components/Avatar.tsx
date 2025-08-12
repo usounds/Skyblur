364cad849c509c05e40ac32e251c343a396694b4
usounds <46260022+usounds@users.noreply.github.com>
2025-08-12 22:50:18 +0900

    feat : アイコンをモノクロに

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -2,6 +2,7 @@
 import { AppBskyActorDefs } from '@atcute/bluesky';
 import Image from 'next/image';
 import Link from 'next/link';
+import { RxAvatar } from "react-icons/rx";
 
 type AvatarProp = {
     userProf: AppBskyActorDefs.ProfileViewDetailed
@@ -27,21 +28,7 @@ export const Avatar: React.FC<AvatarProp> = ({
                     unoptimized
                 />
             ) : (
-                <svg
-                    width="48"
-                    height="48"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="none"
-                    data-testid="userAvatarFallback"
-                >
-                    <circle cx="12" cy="12" r="12" fill="#0070ff"></circle>
-                    <circle cx="12" cy="9.5" r="3.5" fill="#fff"></circle>
-                    <path
-                        fill="#fff"
-                        d="M 12.058 22.784 C 9.422 22.784 7.007 21.836 5.137 20.262 C 5.667 17.988 8.534 16.25 11.99 16.25 C 15.494 16.25 18.391 18.036 18.864 20.357 C 17.01 21.874 14.64 22.784 12.058 22.784 Z"
-                    ></path>
-                </svg>
+                <RxAvatar size={48} />
             )}
 
             <div>
