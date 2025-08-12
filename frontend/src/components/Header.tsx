d18cf644b592dfcec928cf562ba4a405df8a0f3a
usounds <grave.evarg@gmail.com>
2025-08-12 18:55:51 +0900

    fix : ヘッダーが縦方向中央寄せになっていない

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,14 +6,15 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex flex-wrap w-full text-sm h-11 overflow-hidden "
+      <div
+        className="flex flex-wrap w-full text-sm h-11 overflow-hidden items-center"
         style={{
-          minHeight: '50px',  // h-11相当（11 * 4 = 44px）
+          minHeight: '50px', // h-11相当（44px）
           overflow: 'hidden',
         }}
       >
         <nav className="px-4 md:px-8 w-full mx-auto flex justify-between items-center flex-row h-full">
-          <Link href="/" className="text-xl font-semibold ">
+          <Link href="/" className="text-xl font-semibold">
             Skyblur
           </Link>
           <DynamicHeader />
