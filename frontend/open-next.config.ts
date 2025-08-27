559036ca32c204e9796ad12321728c67f17b6862
usounds <46260022+usounds@users.noreply.github.com>
2025-08-27 23:44:38 +0900

    fix : SQS復活

--- frontend/open-next.config.ts
+++ frontend/open-next.config.ts
@@ -6,13 +6,6 @@ const config = {
     // 必要に応じてリージョンを指定
     regions: ["ap-northeast-1"],
 
-    // ISR (revalidate) 設定
-    revalidate: {
-      override: {
-        wrapper: "aws-lambda",     // Lambda 直実行
-        converter: "aws-apigw-v2", // API Gateway v2 経由、SQS を通さない
-      },
-    },
   },
 };
 
