d437b7f8fddeae739c659d7db5b9adc97638285c
usounds <46260022+usounds@users.noreply.github.com>
2026-01-12 20:47:17 +0900

    fix : レイアウト

--- frontend/src/components/PageLoading.tsx
+++ frontend/src/components/PageLoading.tsx
@@ -2,7 +2,7 @@ import { Loading } from "./Loading";
 
 export function PageLoading() {
     return (
-        <div style={{ minHeight: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ minHeight: '72vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Loading />
         </div>
     );
