68ac11e3c78df296cf3f07ed73a32552dc58ccc1
usounds <46260022+usounds@users.noreply.github.com>
2026-01-31 19:13:12 +0900

    fix : ビルドエラー

--- frontend/sst.config.ts
+++ frontend/sst.config.ts
@@ -14,15 +14,19 @@ export default $config({
     const stage = $app.stage;
 
     new sst.aws.Nextjs("Skyblur", {
-      domain: {
-        name: stage === "production"
-          ? "skyblur.uk"
-          : "preview.skyblur.uk",
-        dns: false,
-        cert: stage === "production"
-          ? "arn:aws:acm:us-east-1:036820509199:certificate/76313a88-46d8-48b7-8def-f37776378dc1"
-          : "arn:aws:acm:us-east-1:036820509199:certificate/ee0b076b-692f-4e28-9c05-733cc33d9792",
-      },
+      domain: stage === "production"
+        ? {
+          name: "skyblur.uk",
+          dns: false,
+          cert: "arn:aws:acm:us-east-1:036820509199:certificate/76313a88-46d8-48b7-8def-f37776378dc1"
+        }
+        : stage === "preview"
+          ? {
+            name: "preview.skyblur.uk",
+            dns: false,
+            cert: "arn:aws:acm:us-east-1:036820509199:certificate/ee0b076b-692f-4e28-9c05-733cc33d9792"
+          }
+          : undefined,
       regions: ["ap-northeast-1"],
       transform: {
         server: {
