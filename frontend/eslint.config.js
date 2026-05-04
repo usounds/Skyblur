553867cbdd5eebe46e19c1db164b8f95fe9afc9d
usounds <46260022+usounds@users.noreply.github.com>
2026-05-04 18:53:28 +0900

    lib : アップデート

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,12 +3,24 @@ import coreWebVitals from 'eslint-config-next/core-web-vitals';
 export default [
   ...coreWebVitals,
   {
+    rules: {
+      'react-hooks/immutability': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+  {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
       '@typescript-eslint/no-require-imports': 'error',
     },
   },
   {
-    ignores: ['.sst/**', '.open-next/**']
+    files: ['e2e/**/*.ts'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'off',
+    },
+  },
+  {
+    ignores: ['.sst/**', '.open-next/**', 'coverage/**']
   }
 ];
